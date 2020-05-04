cac950e978354117d0fb5e9f3e1506d1f1b28177
Mayeze Showee <showeemayeze@outlook.fr>
2020-05-04 10:00:24 +0200

    deuxieme etape finis!

--- Activities/Inscription.cs
+++ Activities/Inscription.cs
@@ -7,19 +7,21 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
 
 namespace Demo.Activities
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AiTheme", MainLauncher = false)]
-    public class LoginActivity : Activity
+    [Activity(Label = "@string/app_name", Theme = "@style/AiTheme", MainLauncher= true)]
+    public class Inscription : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            SetContentView(Resource.Layout.connection);
+            SetContentView(Resource.Layout.enregistrement);
+
             // Create your application here
         }
     }
--- Activities/SplashActivity.cs
+++ Activities/SplashActivity.cs
@@ -7,20 +7,26 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
 
 namespace Demo.Activities
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AiTheme", MainLauncher = false)]
-    public class LoginActivity : Activity
+    [Activity(Label = "@strin/app_name", Theme = "@style/AiTheme", MainLauncher = true, NoHistory = false, ScreenOrientation =Android.Content.PM.ScreenOrientation.Portrait)]
+    public class SplashActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            SetContentView(Resource.Layout.connection);
             // Create your application here
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(typeof(Inscription));
         }
     }
 }
